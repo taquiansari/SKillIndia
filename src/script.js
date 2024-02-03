b18c76951633585d9c5f4485a607640ad5eeaf5a
taquiansari <ansaritaqui007@gmail.com>
2024-02-03 21:01:58 +0530

    made sign in and register interface

--- src/script.js
+++ src/script.js
@@ -4,8 +4,8 @@ const registerLink = document.querySelector('.register-link');
 
 registerLink.addEventListener('click', () => {
     logregBox.classList.add('active');
-})
+});
 
 loginLink.addEventListener('click', () => {
     logregBox.classList.remove('active');
-})
+});
