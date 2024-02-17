0adbda4474ea4f1903a9a2e56981b8c0263fbbc0
taquiansari <ansaritaqui007@gmail.com>
2024-02-18 01:35:06 +0530

    Improved UI

--- core/views.py
+++ core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import redirect, render, HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from datetime import datetime
+from core.models import Contact
 
 # Create your views here.
 
@@ -18,6 +20,13 @@ def services(request):
     return render(request, 'services.html')
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc, date=datetime.today())
+        contact.save()
     return render(request, 'contact.html')
 
 def community(request):
--- skillindia/settings.py
+++ skillindia/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 'core.apps.CoreConfig',
     'core',
     'django.contrib.admin',
     'django.contrib.auth',
